711e4010bb97ca4b141ee291fc98ad02e2522619
wesley <wesley.gomes222@hotmail.com>
2023-03-25 11:35:46 -0300

    variações do catch

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -2,7 +2,17 @@ public class Fluxo {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
+        try {
         metodo1();
+        } catch(ArithmeticException | NullPointerException ex) { 
+        	// ex é uma referência para a classe 'ArithmeticException' e 'NullPointerException'
+        	String msg = ex.getMessage();
+        	// o método 'getMesssage()' exibe a mensagem da exceção.
+        	
+        	System.out.println("Exception " + msg);
+        	ex.printStackTrace();
+        	// irá mostrar o número da linha onde se encontra o erro e o nome da classe.
+        }
         System.out.println("Fim do main");
     }
 
@@ -15,13 +25,10 @@ public class Fluxo {
     private static void metodo2() {
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
-            System.out.println(i);
-            
-            try {
+            System.out.println(i); 
             int a = i / 0;
-            } catch(ArithmeticException ex) {
-            	System.out.println("ArithmeticException");
-            }
+            //Conta c = null;
+            //c.deposita();
         }
         System.out.println("Fim do metodo2");
     }
